f8e6a16e116ac3a5ce9b489282241665b0788386
Higor Felipe <higorfelipe127@gmail.com>
2020-03-05 08:19:45 -0300

    Creep following Nexus

--- Assets/Scripts/Caracters/PlayerController.cs
+++ Assets/Scripts/Caracters/PlayerController.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class PlayerController : MonoBehaviour
 {
     NavMeshAgent agent;
     Camera cam;
     public LayerMask walkableLayer;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
         cam = Camera.main;
