2b5921f4e77fe17c674bc8cc0b2ae7f83664b97b
Higor Felipe <higorfelipe127@gmail.com>
2020-03-05 10:15:23 -0300

    Creep spawn

--- Assets/Scripts/Characters/CreepController.cs
+++ Assets/Scripts/Characters/CreepController.cs
@@ -6,10 +6,15 @@ using UnityEngine.AI;
 public class CreepController : MonoBehaviour
 {
     NavMeshAgent agent;
-   public GameObject finalTarget;
+   GameObject finalTarget;
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        GameObject obj = GameObject.FindWithTag("Finish");
+        if (obj != null)
+        {
+            finalTarget = obj;
+        }
     }
 
 
