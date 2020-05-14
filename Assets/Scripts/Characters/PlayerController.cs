6e2e4c93242848cf9a94dbb5c828f71f013e0d53
Eduardo José <eduardoaranhanx1@gmail.com>
2020-05-14 01:51:08 -0300

    Collectible Layer

--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : TouchableGameObject
     NavMeshAgent agent;
     Camera cam;
     public LayerMask walkableLayer;
+    public LayerMask collectibleLayer;
 
  
 
@@ -31,6 +32,12 @@ public class PlayerController : TouchableGameObject
 
             if(Physics.Raycast(ray, out hit, Mathf.Infinity, walkableLayer))
             {
+                agent.SetDestination(hit.point);
+            }
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, collectibleLayer))
+            {
+                Debug.Log("Collectible: " + hit.collider.name);
                 agent.SetDestination(hit.point);
             }
         }
